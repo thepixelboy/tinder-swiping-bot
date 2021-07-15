05215c7e6f2429b2ef8e6c3f1ed14adb66a42b88
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-07-15 17:02:30 +0200

    feat: navigate to login page

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from selenium import webdriver
+from time import sleep
+from dotenv import load_dotenv
+
+FB_EMAIL = os.getenv("FB_EMAIL")
+FB_PASSWORD = os.getenv("FB_PASSWORD")
+
+chrome_driver_path = os.getenv("CHROME_DRIVER_PATH")
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get("http://www.tinder.com")
+
+sleep(2)
+login_button = driver.find_element_by_xpath(
+    '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/header/div[1]/div[2]/div/button'
+)
+login_button.click()
+
+sleep(2)
+fb_login = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+fb_login.click()
